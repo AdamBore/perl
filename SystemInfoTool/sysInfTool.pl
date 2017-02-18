bc5ba5f18a10d412211596107a1a4ad5030382bf
AdamBore <adam200il@gmail.com>
2017-02-18 21:22:39 +0200

    bla

---
+++ SystemInfoTool/sysInfTool.pl
@@ -0,0 +1,52 @@
+use strict;
+use warnings;
+use 5.010;
+
+use Win64::SystemInfo;
+use Data::Dumper qw(Dumper); #for debug
+
+my $logFileName = shift or die "Usage is: %0 LOG_FILE_NAME"; # make sure we got a target file.
+
+open( my $logFileHandle, ">" , $logFileName ) or die "Cannot open log file: '$logFileName'."; #make sure we have a place to write to.
+
+my %cpuDataHash = GetCpuInfo();
+my %memoryDataHash = GetMemoryInfo();
+# print Dumper \%cpuDataHash; #for debug
+PrintHash( %cpuDataHash );
+PrintHash( %memoryDataHash );
+
+close $logFileHandle;
+
+sub GetMemoryInfo {
+  my %memoryDataHash;
+  Win64::SystemInfo::MemoryStatus( %memoryDataHash,"MB" );
+  return %memoryDataHash;
+}
+
+sub PrintHash {
+    my %_hashForPrinting = @_;
+    # print Dumper \%_hashForPrinting; #for debug
+    if( %_hashForPrinting ){
+        foreach my $key ( sort keys %_hashForPrinting ){
+            print "$key $_hashForPrinting{$key}.\n";
+        }
+    }
+}
+
+
+
+sub GetCpuInfo {
+    my %cpuDataHash;
+    Win64::SystemInfo::ProcessorInfo( %cpuDataHash );
+
+    my %parsedCPUData;
+    $parsedCPUData{ NumProcessors } = $cpuDataHash{ NumProcessors };
+
+    foreach my $key ( keys %{$cpuDataHash{ Processor0 }} )
+    {
+        # print "$key = $cpuDataHash{ Processor0 }{$key}\n"; #for debug
+        $parsedCPUData{ $key } = $cpuDataHash{ Processor0 }{$key};
+    }
+    # print Dumper \%parsedCPUData; #for debug
+    return %parsedCPUData;
+}
