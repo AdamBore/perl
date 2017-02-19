f6745ecffcec339b0f5b98a58061b2b11657a65c
AdamBore <adam200il@gmail.com>
2017-02-19 15:15:12 +0200

    finished html builder phase1

--- SystemInfoTool/myHtmlCreator.pm
+++ SystemInfoTool/myHtmlCreator.pm
@@ -7,15 +7,28 @@ use Exporter qw( import );
 our @EXPORT_OK = qw( GenerateSystemHtml );
 
 sub GenerateSystemHtml {
-    my $targetFileHandle = shift or die "No Target file to write HTML report to.";
-    my %dataToInsert = shift or return;
+
+    #test function parameters
+    my $_targetFileHandle = shift or die "No Target file to write HTML report to.";
+    my %_dataToInsert = shift or return;
     my $fileInAString;
     {
+        #open a source master file
+        my $sourceFileName = "htmlMaster.html";
+        open ( my $sourceFileHandle, "<" , $sourceFileName ) or die "Cannot open '$sourceFileName'.";
+
+        # put file in string
         local $/ = undef;
-        $fileInAString = <$targetFileHandle>;
+        $fileInAString = <$sourceFileHandle>;
+        close $sourceFileHandle;
     }
-    # put file in string
+
     # perform subsitutions.
-    # return new string.
-    return "blabla";
+    foreach my $key ( keys %_dataToInsert ) {
+        $fileInAString =~ s/$key/$_dataToInsert{ $key }/g;
+    }
+
+    # write new file.
+    print $_targetFileHandle $fileInAString;
+    return;
 }
